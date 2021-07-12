a0a176274853128d1ac3ec788c625e750ffc17c3
hienthan <hienthan@cleverhub.vn>
2021-07-12 23:02:02 +0700

    Added the signup, login components Successful signup and login, completed part 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import {AuthModule} from './auth/auth.module';
+import {SignupComponent} from './auth/signup/signup.component';
+import {LoginComponent} from './auth/login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgxWebstorageModule} from 'ngx-webstorage';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,8 @@ import {AuthModule} from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    NgxWebstorageModule.forRoot(),
     AuthModule
   ],
   providers: [],
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,6 +2,10 @@ import { Injectable, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SignupRequestPayload } from '../signup/singup-request.payload';
 import { Observable, throwError } from 'rxjs';
+import {LocalStorageService} from 'ngx-webstorage';
+import {LoginRequestPayload} from '../login/login-request.payload';
+import {LoginResponse} from '../login/login-response.payload';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +15,21 @@ export class AuthService {
   @Output() loggedIn: EventEmitter<boolean> = new EventEmitter();
   @Output() username: EventEmitter<string> = new EventEmitter();
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) {
   }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, { responseType: 'text' });
+  }
+
+  login(loginRequestPayload: LoginRequestPayload) : Observable<boolean> {
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload)
+      .pipe(map(data => {
+        this.localStorage.store('authenticationToken', data.authenticationToken);
+        this.localStorage.store('username', data.username);
+        this.localStorage.store('refreshToken', data.refreshToken);
+        this.localStorage.store('expiredAt', data.expiresAt);
+        return true;
+      }))
   }
 }
